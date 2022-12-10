4257f600c4708e96567279b6825d58ef385b0c41
Souleymane Sy <souleymanesyservices@gmail.com>
2022-12-10 19:33:58 +0100

    Layout Finish

---
+++ src/script.js
@@ -0,0 +1,14 @@
+
+const showMenu = (hamburgerMenu, navItems) => {
+    const hamburger = document.querySelector(hamburgerMenu);
+    const nav = document.querySelector(navItems);
+
+    if (hamburger && nav) {
+        hamburger.addEventListener('click', () => {
+            nav.classList.toggle('active');
+            hamburger.classList.toggle('active')
+        })
+    }
+};
+
+showMenu('#hamburger-menu', '#mobile-menu');
